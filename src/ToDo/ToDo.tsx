d50bf8d573953d4e24f08f81d8ef779250e788e3
MaxboDev <maxborkowski@gmail.com>
2024-12-02 17:21:20 +0000

    style improvements

--- src/ToDo/ToDo.tsx
+++ src/ToDo/ToDo.tsx
@@ -2,11 +2,9 @@ import './ToDo.css';
 
 export default function ToDo() {
   return (
-    <>
-      <div className="input-wrapper">
-        <input className="add-task-textbox" type="text" placeholder="Enter a new task name..." />
-        <button className="add-task-button">Add</button>
-      </div>
-    </>
+    <div className="input-wrapper">
+      <input className="add-task-textbox" type="text" placeholder="Enter a new task name..." />
+      <button className="add-task-button">Add</button>
+    </div>
   )
 }
